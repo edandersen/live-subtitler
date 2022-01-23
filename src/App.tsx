4ce4a97c14aadf8da98fe7a0d80807565a2a249c
Ed Andersen <mail@edandersen.com>
2022-01-23 18:55:11 +0000

    Added bootstrap instead of tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,93 @@
-import React from 'react'
-import logo from './logo.svg'
-import tauriCircles from './tauri.svg'
-import tauriWord from './wordmark.svg'
-import './App.css'
+import { Deepgram } from "@deepgram/sdk";
+import React, { ChangeEvent, ChangeEventHandler, Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="inline-logo">
-          <img src={tauriCircles} className="App-logo rotate" alt="logo" />
-          <img src={tauriWord} className="App-logo smaller" alt="logo" />
-        </div>
-        <a
-          className="App-link"
-          href="https://tauri.studio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tauri
-        </a>
-        <img src={logo} className="App-logo rotate" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  )
+interface AppProps {}
+
+interface AppState {
+  apiKey: string;
 }
 
-export default App
+class App extends Component<AppProps, AppState> {
+  private deepgram: Deepgram | undefined;
+
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      apiKey: "",
+    };
+
+    this.handleApiKeyChange = this.handleApiKeyChange.bind(this);
+  }
+
+  handleApiKeyChange(event: ChangeEvent<HTMLInputElement>) {
+    const apiKey = event.target.value;
+    this.setState({ apiKey: apiKey });
+    if (apiKey?.length == 40) {
+      this.setupDeepgram(apiKey);
+    }
+  }
+
+  setupDeepgram(apiKey: string) {
+    this.deepgram = new Deepgram(apiKey);
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      const mediaRecorder = new MediaRecorder(stream, {
+        mimeType: "audio/webm",
+      });
+
+      if (this.deepgram != undefined) {
+        const deepgramSocket = this.deepgram.transcription.live({
+          punctuate: true,
+          version: "latest",
+        });
+
+        deepgramSocket.addListener("open", () => {
+          mediaRecorder.addEventListener("dataavailable", async (event) => {
+            if (event.data.size > 0 && deepgramSocket.getReadyState() == 1) {
+              deepgramSocket.send(event.data);
+            }
+          });
+          mediaRecorder.start(1000);
+        });
+
+        deepgramSocket.addListener("transcriptReceived", (received) => {
+          const transcript = received.channel.alternatives[0].transcript;
+          if (transcript && received.is_final) {
+            console.log(transcript);
+          }
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <header className="bg-white shadow">
+          <h1>Subtitler</h1>
+        </header>
+        <main>
+          <span className="text-gray-700">API Key</span>
+          <input
+            type="password"
+            maxLength={40}
+            className="
+                    mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-gray-300
+                    shadow-sm
+                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                  "
+            value={this.state.apiKey}
+            onChange={this.handleApiKeyChange}
+            placeholder="Enter deepgram API key"
+          />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
